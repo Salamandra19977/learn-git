db468bdf910053f2ceae0e2e6d7afff128e57153
Salamandra19977 <salamandra199720@gmail.com>
2024-10-29 16:51:52 +0200

    last

--- main.js
+++
@@ -1,9 +0,0 @@
-function block1() {
-	let elem = document.getElementById('block1')
-	elem.style.background = `rgb(${rnd(255)}, ${rnd(255)}, ${rnd(255)})`
-	elem.style.borderRadius = '25%'
-}
-
-function rnd(arg) {
-	return Math.floor(Math.random() * arg)
-}
